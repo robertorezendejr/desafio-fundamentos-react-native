92288f4d5fc9f545170c1df90374f274c13fd879
Roberto Rezende Jr <robertorezendejr@hotmail.com>
2021-07-11 22:35:23 -0300

    finish

---
+++ ios/Pods/Target Support Files/Pods-GoMarketplace-GoMarketplaceTests/Pods-GoMarketplace-GoMarketplaceTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_GoMarketplace_GoMarketplaceTests : NSObject
+@end
+@implementation PodsDummy_Pods_GoMarketplace_GoMarketplaceTests
+@end
---
+++ ios/Pods/Target Support Files/Pods-GoMarketplace/Pods-GoMarketplace-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_GoMarketplace : NSObject
+@end
+@implementation PodsDummy_Pods_GoMarketplace
+@end
